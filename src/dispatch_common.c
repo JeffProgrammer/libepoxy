efd873c105b445228460f62f0db842967c0992ec
Daniel Wolf <nephatrine@gmail.com>
2015-08-17 11:25:39 -0400

    correct MSVC size_t to int warning

--- src/dispatch_common.c
+++ src/dispatch_common.c
@@ -369,7 +369,7 @@ bool
 epoxy_extension_in_string(const char *extension_list, const char *ext)
 {
     const char *ptr = extension_list;
-    int len = strlen(ext);
+    size_t len = strlen(ext);
 
     /* Make sure that don't just find an extension with our name as a prefix. */
     while (true) {
