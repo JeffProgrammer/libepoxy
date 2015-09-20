61905c9601c9570d4c51db97a2f08aa223fb51c1
Daniel Wolf <nephatrine@gmail.com>
2015-09-20 17:13:55 -0400

    changed GLES cmake macros

--- CMake/modules/FindGLESv1.cmake
+++ CMake/modules/FindGLESv1.cmake
@@ -10,7 +10,6 @@ set(GLESv1_NAMES ${GLESv1_NAMES} GLESv1_CM libGLES_CM)
 find_library(GLESv1_LIBRARY NAMES ${GLESv1_NAMES} PATHS /opt/vc/lib)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GLESv1 DEFAULT_MSG GLESv1_LIBRARY GLESv1_INCLUDE_DIR)
+find_package_handle_standard_args(GLES_V1 DEFAULT_MSG GLESv1_LIBRARY GLESv1_INCLUDE_DIR)
 
 mark_as_advanced(GLESv1_INCLUDE_DIR GLESv1_LIBRARY)
-
--- CMake/modules/FindGLESv2.cmake
+++ CMake/modules/FindGLESv2.cmake
@@ -10,7 +10,6 @@ set(GLESv2_NAMES ${GLESv2_NAMES} GLESv2 libGLESv2)
 find_library(GLESv2_LIBRARY NAMES ${GLESv2_NAMES} PATHS /opt/vc/lib)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GLESv2 DEFAULT_MSG GLESv2_LIBRARY GLESv2_INCLUDE_DIR)
+find_package_handle_standard_args(GLES_V2 DEFAULT_MSG GLESv2_LIBRARY GLESv2_INCLUDE_DIR)
 
 mark_as_advanced(GLESv2_INCLUDE_DIR GLESv2_LIBRARY)
-
